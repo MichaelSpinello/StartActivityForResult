95be78d6f266f592719c794e690582264016bd06
Michael Spinello <spinellomichael@yahoo.it>
2020-02-11 16:14:52 +0100

    commit dopo aver messo spinner, messaggio e tutto

--- app/src/main/java/com/michael/startactivityforresult/MainActivity.java
+++ app/src/main/java/com/michael/startactivityforresult/MainActivity.java
@@ -6,10 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.FileProvider;
 import androidx.savedstate.SavedStateRegistry;
-import androidx.savedstate.SavedStateRegistryController;
-import androidx.savedstate.SavedStateRegistryOwner;
-
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
@@ -17,53 +15,52 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.PersistableBundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static android.os.Environment.DIRECTORY_PICTURES;
-import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private Button mUpload;
     private TextView mDescription;
+    private TextView mStatus;
     private ImageView mImageView;
     private Bitmap mFoto;
+    private Bitmap mFotoToUpload = null;
+    private ProgressBar mSpinner;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Intent takePictureIntent;
-    private String mPhotoDirectory = "";
+    private String mPhotoDirectory;
     private static final String IMAGE_KEY = "img";
     private static final String DESCRIPTION = "dsc";
+    private static final String STATUS = "sts";
 
     private final String MY_SAVED_STATE_KEY = "my_saved_state";
-    //private final String SOME_VALUE_KEY = "some_value";
-   // private String someValue;
 
     SavedStateRegistry.SavedStateProvider savedStateProvider =
             new SavedStateRegistry.SavedStateProvider() {
         @NonNull
         @Override
         public Bundle saveState() {
-            //saveState().putString(SOME_VALUE_KEY, someValue);
             Bundle bundle = new Bundle();
             if(mPhotoDirectory!= null && mDescription != null) {
-
                 bundle.putString(IMAGE_KEY, mPhotoDirectory);
                 bundle.putString(DESCRIPTION, (String) mDescription.getText());
+                bundle.putString(STATUS, (String)mStatus.getText());
             }
             return bundle;
         }
@@ -80,73 +77,102 @@ public class MainActivity extends AppCompatActivity {
         mButton = findViewById(R.id.button);
         mUpload = findViewById(R.id.upload);
         mDescription = findViewById(R.id.description);
+        mStatus = findViewById(R.id.status);
         mImageView = findViewById(R.id.imageview);
+        mSpinner = findViewById(R.id.progressBar);
+        mSpinner.setVisibility(View.GONE);
+        final ConstraintLayout mLayout = findViewById(R.id.root_layout);
+        ViewTreeObserver vto = mLayout.getViewTreeObserver();
+
         if(savedInstanceState != null) {
-            //someValue = getSavedStateRegistry().consumeRestoredStateForKey(MY_SAVED_STATE_KEY)
-             //        .getString(SOME_VALUE_KEY);
             Bundle restoredState = getSavedStateRegistry().consumeRestoredStateForKey(MY_SAVED_STATE_KEY);
             if(restoredState != null){
                 mPhotoDirectory = restoredState.getString(IMAGE_KEY);
                 mDescription.setText(restoredState.getString(DESCRIPTION));
+                mStatus.setText(restoredState.getString(STATUS));
             }
-
-           // mPhotoDirectory = savedInstanceState.getString(IMAGE_KEY);
-            //mDescription.setText(savedInstanceState.getString(DESCRIPTION));
         }
-
 
         mButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
-
         });
 
-        final ConstraintLayout layout = findViewById(R.id.root_layout);
-        ViewTreeObserver vto = layout.getViewTreeObserver();
+
         if(mPhotoDirectory != null) {
             vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
                 @Override
                 public void onGlobalLayout() {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                        layout.getViewTreeObserver()
+                        mLayout.getViewTreeObserver()
                                 .removeOnGlobalLayoutListener(this);
                     } else {
-                        layout.getViewTreeObserver()
+                        mLayout.getViewTreeObserver()
                                 .removeGlobalOnLayoutListener(this);
                     }
                     setPic(mPhotoDirectory);
                 }
             });
-
-            mUpload.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    new UploadTask().execute((Void)null);
-                }
-            });
         }
+
     }
 
+    private void startUploadButton(){
+        final RotateAnimation rotate = new RotateAnimation(0, 180,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new LinearInterpolator());
+        rotate.setDuration(10000);
+        mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UploadTask uploadTask = new UploadTask();
+                uploadTask.setOnUploadTaskListener(MainActivity.this, new UploadTask.OnUploadTaskListener() {
+                    @Override
+                    public void onSuccessReceiver(int upload, Boolean inProgress) {
+                        if(inProgress) {
+                            mSpinner.setVisibility(View.VISIBLE);
+                            mSpinner.startAnimation(rotate);
+                        }
+                        else{
+                            mSpinner.setVisibility(View.INVISIBLE);
+                        }
+                        switch(upload){
+                            case -1:
+                                mStatus.setText("NON caricata");
+                                break;
+                            case 0:
+                                mStatus.setText("uploading:");
+                                break;
+                            case 1:
+                                mStatus.setText("caricata");
+                                break;
+                        }
+                    }
+                });
+                uploadTask.execute((Bitmap)mFotoToUpload);
+            }
+        });
+    }
 
     private void dispatchTakePictureIntent(){
-            takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if(photoFile != null){
-                    Uri photoURI = FileProvider.getUriForFile(this,
-                            "com.michael.startactivityforresult", photoFile);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
+            if(photoFile != null){
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.michael.startactivityforresult", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
     }
 
     @Override
@@ -185,17 +211,13 @@ public class MainActivity extends AppCompatActivity {
             switch (orientation) {
                 case ExifInterface.ORIENTATION_ROTATE_270:
                     angle = 270f;
-
                     break;
                 case ExifInterface.ORIENTATION_ROTATE_180:
                     angle = 180f;
-
                     break;
                 case ExifInterface.ORIENTATION_ROTATE_90:
                     angle = 90f;
-
                     break;
-
                 case ExifInterface.ORIENTATION_NORMAL:
                     angle = 0f;
                     break;
@@ -211,30 +233,10 @@ public class MainActivity extends AppCompatActivity {
             mFoto = Bitmap.createScaledBitmap(mFoto, mImageView.getWidth(),
                     mImageView.getHeight(), true);
             mImageView.setImageBitmap(mFoto);
+            mFotoToUpload = mFoto;
+            startUploadButton();
 
         }
     }
-
-
-    /*@Override
-    protected void onPause() {
-        super.onPause();
-       // Bundle savedInstanceState = takePictureIntent.getExtras();
-        //String tmp = savedInstanceState.getString(MediaStore.EXTRA_OUTPUT);
-        //onSaveInstanceState(MainActivity, mPhotoDirectory);
-        Bundle bundle = new Bundle();
-        bundle.putString("foto_acquisita", mPhotoDirectory);
-        onSaveInstanceState(bundle);
-    }*/
-
-   /* @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        savedStateProvider.saveState();*/
-       // someValue = getSavedStateRegistry().consumeRestoredStateForKey(MY_SAVED_STATE_KEY)
-       //         .getString(SOME_VALUE_KEY);
-        //outState.putString(IMAGE_KEY, mPhotoDirectory);
-       // outState.putString(DESCRIPTION, (String)mDescription.getText());
-    //}
 
 }
--- app/src/main/java/com/michael/startactivityforresult/UploadTask.java
+++ app/src/main/java/com/michael/startactivityforresult/UploadTask.java
@@ -1,17 +1,63 @@
 package com.michael.startactivityforresult;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
-public class UploadTask extends AsyncTask <Void, Void, Void>{
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
 
-    @Override
-    protected Void doInBackground(Void... voids) {
-        return null;
-    }
+public class UploadTask extends AsyncTask <Bitmap, Void, Boolean>{
+
+    protected Boolean inProgress;
+    private MainActivity context;
+    private OnUploadTaskListener listener;
+    private int upload;
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        inProgress = true;
+        upload = 0;
+        listener.onSuccessReceiver(upload, inProgress);
     }
 
+    @Override
+    protected Boolean doInBackground(Bitmap... bitmaps) {
+        try {
+            URL url = new URL("https://www.dropbox.com/it/");
+            Thread.sleep(10000);
+            //InputStream inputStream = url.openConnection().getInputStream();
+            return new Random().nextBoolean();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean success) {
+        super.onPostExecute(success);
+        if(success)
+            upload = 1;
+        else
+            upload = -1;
+        inProgress = false;
+        listener.onSuccessReceiver(upload, inProgress);
+    }
+
+    public void setOnUploadTaskListener(MainActivity context, OnUploadTaskListener listener){
+        this.listener = listener;
+        this.context = context;
+    }
+
+    public interface OnUploadTaskListener{
+        void onSuccessReceiver(int upload, Boolean inProgress);
+    }
 }
